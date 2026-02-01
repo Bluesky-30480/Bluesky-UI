ccc9ad9afedffb6f35ddf280d691cd08fa5bb185
Ultimate Focus Developer <developer@ultimatefocus.app>
2026-02-01 17:30:31 +1000

    Reworked workflow to all files

--- showcaser/src/main.tsx
+++ showcaser/src/main.tsx
@@ -5,9 +5,12 @@ import { BlueskyUIProvider } from '@bluesky-ui/ui'
 import App from './App'
 import './styles/index.css'
 
+// Use basename for GitHub Pages deployment
+const basename = import.meta.env.BASE_URL
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <BlueskyUIProvider defaultTheme="blue">
         <App />
       </BlueskyUIProvider>
