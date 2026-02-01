ff688ba17f1c0d2d903c9fa144b7be18ef679a7f
Ultimate Focus Developer <developer@ultimatefocus.app>
2026-02-01 17:37:18 +1000

    Fix: Use HashRouter for GitHub Pages

--- showcaser/src/main.tsx
+++ showcaser/src/main.tsx
@@ -1,19 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { BlueskyUIProvider } from '@bluesky-ui/ui'
 import App from './App'
 import './styles/index.css'
 
-// Use basename for GitHub Pages deployment (strip trailing slash)
-const basename = import.meta.env.BASE_URL.replace(/\/$/, '')
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename={basename || '/'}>
+    <HashRouter>
       <BlueskyUIProvider defaultTheme="blue">
         <App />
       </BlueskyUIProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
